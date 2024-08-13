c0c704275b880116ab4014d3c367a2278209cbcc
sonnguyen181004 <snthanh18102004@gmail.com>
2024-08-13 12:23:14 +0700

    Add files via upload

---
+++ Demo_Java/FindSecondMax/src/FindSecondMax.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class FindSecondMax {
+    public static int findsecondmax(int []arr){
+    int max=Integer.MIN_VALUE;
+    int secondmax=Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+    if(arr[i]>max){
+     secondmax=max;
+     max=arr[i];
+    }
+    else if (arr[i]<max&&arr[i]!=max){
+     secondmax=arr[i];
+    }
+    }
+    return secondmax;
+    }
+    
+    public static void main(String[] args) {
+        int[]arr={1,3,9,10,1999,3451};
+        int secondmax=findsecondmax(arr);
+        System.out.println(secondmax);
+        
+    }
+}
---
+++ Demo_Java/MoveZeroes/src/MoveZeroes.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class MoveZeroes {
+   public void printArray(int[]arr){
+ int n=arr.length;
+ for(int i=0;i<n;i++){
+     System.out.print(arr[i]+" ");
+ }
+       System.out.println(" ");
+}
+   public void arrayDemo(){
+   int []arr={8,1,0,2,1,0,3};
+       printArray(arr);
+       moveZeros(arr,arr.length);
+       printArray(arr);
+   }
+   public void moveZeros(int[]arr,int n){
+   int j=0;
+   for(int i=0;i<n;i++){
+   if(arr[i]!=0&&arr[j]==0){
+    int temp= arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    
+   }
+   if(arr[j]!=0){
+   j++;
+   }
+   }
+   
+  
+   }
+    public static void main(String[] args) {
+        MoveZeroes arr1=new MoveZeroes();
+        arr1.arrayDemo();
+    }
+
+}
