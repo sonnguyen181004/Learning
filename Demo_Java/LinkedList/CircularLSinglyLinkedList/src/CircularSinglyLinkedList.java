a9d9f04ef26012d17a496a3eb8a99f4fe9f07d9f
sonnguyen181004 <snthanh18102004@gmail.com>
2024-08-31 10:44:51 +0700

    Update CircularSinglyLinkedList.java

--- Demo_Java/LinkedList/CircularLSinglyLinkedList/src/CircularSinglyLinkedList.java
+++ Demo_Java/LinkedList/CircularLSinglyLinkedList/src/CircularSinglyLinkedList.java
@@ -9,35 +9,61 @@
  * @author PC
  */
 public class CircularSinglyLinkedList {
+
     private ListNode last;
     private int length;
-    private class ListNode{
-    private ListNode next;
-    private int data;
-        public ListNode(int data){
-    this.data=data;
+
+    private class ListNode {
+
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
     }
+
+    public CircularSinglyLinkedList() {
+        last = null;
+        length = 0;
     }
-    public  CircularSinglyLinkedList(){
-    last=null;
-    length=0;
+
+    public int length() {
+        return length;
     }
-    public int length(){
-    return length;
+
+    public boolean isEmpty() {
+        return length == 0;
     }
-    public boolean isEmpty(){
-    return length==0;
+
+    public void createCircularSinlyLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(6);
+        ListNode fourth = new ListNode(12);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+        last = fourth;
     }
-    public void createCircularSinlyLinkedList(){
-    ListNode first=new ListNode(1);
-        ListNode second=new ListNode(2);
-    ListNode third=new ListNode(6);
-    ListNode fourth=new ListNode(12);
-first.next=second;
-second.next=third;
-third.next=fourth;
-fourth.next=first;
-last=fourth;
+
+    public void display() {
+        if (last == null) {
+            return;
+        }
+        ListNode first = last.next;
+        while (first != last) {
+            System.out.print(first.data + " ");
+            first = first.next;
+        }
+        System.out.print(first.data);
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList cll = new CircularSinglyLinkedList();
+        cll.createCircularSinlyLinkedList();
+        cll.display();
     }
 
 }
