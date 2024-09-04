771fc2ba726bca445089e78d576d18b5b57cd449
sonnguyen181004 <snthanh18102004@gmail.com>
2024-09-04 23:26:35 +0700

    Update Queue.java

--- Demo_Java/Queue/src/Queue.java
+++ Demo_Java/Queue/src/Queue.java
@@ -9,53 +9,97 @@
  * @author PC
  */
 public class Queue {
+
     private ListNode front;
     private ListNode rear;
     private int length;
-    public Queue(){
-    this.front=null;
-    this.rear=null;
-    this.length=0;
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
     }
-    public class ListNode{
-    private int data;
-    private ListNode next;
-    
-    public ListNode(int data){
-    this.data=data;
-    this.next=null;
+
+    public class ListNode {
+
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+
+    public int length() {
+        return length;
     }
-    public int length(){
-    return length;}
-    public boolean isEmpty(){
-    return length== 0;
+
+    public boolean isEmpty() {
+        return length == 0;
     }
-    public void enqueue(int data){
-    ListNode tmp= new ListNode(data);
-    if(isEmpty()){
-    front=tmp;
-    }else{
-    rear.next=tmp;
+
+    public void enqueue(int data) {
+        ListNode tmp = new ListNode(data);
+        if (isEmpty()) {
+            front = tmp;
+        } else {
+            rear.next = tmp;
+        }
+        rear = tmp;
+        length++;
     }
-    rear=tmp;
-    length++;
+
+    public int dequeue() {
+        if (isEmpty()) {
+            return length;
+        }
+        int result = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        length--;
+        return result;
     }
-    public void print(){
-    if(isEmpty()){
-    return;
-    }
-    ListNode current=front;
-    while(current!=null){
-        System.out.print(current.data+"-->");
-        current=current.next;
-    }
+
+    public void print() {
+        if (isEmpty()) {
+            return;
+        }
+        ListNode current = front;
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
         System.out.println("Null");
     }
+
+    public int first() {
+        if (isEmpty()) {
+            return length;
+        }
+        return front.data;
+    }
+
+    public int last() {
+        if (isEmpty()) {
+            return length;
+        }
+        return rear.data;
+    }
+
     public static void main(String[] args) {
-        Queue queue =new Queue();
+        Queue queue = new Queue();
         queue.enqueue(10);
         queue.enqueue(40);
+        queue.enqueue(102);
         queue.print();
+        System.out.println("queue dequeue");
+        queue.dequeue();
+        queue.print();
+        System.out.println(queue.first());
+        System.out.println(queue.last());
+
     }
 }
